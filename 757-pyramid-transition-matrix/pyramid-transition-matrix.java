a923b6de07af749c65dbca2282235698eceaff2c
rishissingh <rishipratap6698@gmail.com>
2025-12-29 08:41:17 +0530

    Time: 3 ms (98.09%) | Memory: 44.1 MB (84.71%) - LeetSync

--- 757-pyramid-transition-matrix/pyramid-transition-matrix.java
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -1,47 +1,91 @@
 class Solution {
-    Map<String, List<Character>> map = new HashMap<>();
-
     public boolean pyramidTransition(String bottom, List<String> allowed) {
-        // Step 1: Build the mapping
-        for (String s : allowed) {
-            String key = s.substring(0, 2);
-            char val = s.charAt(2);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(val);
-        }
-        // Step 2: Start DFS
-        return dfs(bottom);
+        return new Solver(allowed, bottom.length()).canDo(bottom);
     }
 
-    private boolean dfs(String bottom) {
-        // Base case: pyramid completed
-        if (bottom.length() == 1) return true;
+    static final class Solver {
+        final int[][] cache;
+        final int[][][] allowed = new int[6][6][];
 
-        // Generate all possible next rows
-        List<String> nextRows = new ArrayList<>();
-        buildNextRow(bottom, 0, new StringBuilder(), nextRows);
-
-        // Try each next row
-        for (String next : nextRows) {
-            if (dfs(next)) return true;
+        public Solver(List<String> allowed, int len) {
+            final int[][] cnt = new int[6][6];
+            for (String a : allowed) {
+                cnt[a.charAt(0) - 'A'][a.charAt(1) - 'A']++;
+            }
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    this.allowed[i][j] = new int[cnt[i][j]];
+                }
+            }
+            this.cache = new int[len][];
+            int size = 6 * 6;
+            for (int i = 3; i < len; i++) {
+                cache[i] = new int[size *= 6];
+            }
+            for (String a : allowed) {
+                final int first = a.charAt(0) - 'A';
+                final int second = a.charAt(1) - 'A';
+                final int third = a.charAt(2) - 'A';
+                this.allowed[first][second][--cnt[first][second]] = third;
+            }
         }
-        return false;
-    }
 
-    private void buildNextRow(String bottom, int index,
-                              StringBuilder current, List<String> result) {
-
-        if (index == bottom.length() - 1) {
-            result.add(current.toString());
-            return;
+        boolean canDo(String s) {
+            final int[] arr = new int[s.length()];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = s.charAt(i) - 'A';
+            }
+            return s.length() == 2 ? allowed[arr[0]][arr[1]].length > 0 : compute(arr, arr.length);
         }
 
-        String key = bottom.substring(index, index + 2);
-        if (!map.containsKey(key)) return;
+        boolean canDo(int[] arr, int len) {
+            if (len == 2) {
+                return allowed[arr[0]][arr[1]].length > 0;
+            } else {
+                final var key = encode(arr, len);
+                final var cached = cache[len][key];
+                if (cached != 0) {
+                    return cached == 1;
+                }
+                final var result = compute(arr, len);
+                cache[len][key] = result ? 1 : 2;
+                return result;
+            }
+        }
+        
+        private int encode(int[] arr, int len) {
+            int r = arr[0];
+            for (int i = 1; i < len; i++) {
+                r = r * 6 + arr[i];
+            }
+            return r;
+        }
 
-        for (char c : map.get(key)) {
-            current.append(c);
-            buildNextRow(bottom, index + 1, current, result);
-            current.deleteCharAt(current.length() - 1); // backtrack
+        boolean compute(int[] arr, int len) {
+            for (int i = 2; i < len; i++) {
+                if (allowed[arr[i - 1]][arr[i]].length == 0) {
+                    return false;
+                }
+            }
+            return compute(new int[len], 0, arr, 0, len);
+        }
+
+        boolean compute(int[] prefix, int plen, int[] suffix, int sidx, int slen) {
+            if (plen > 1 && !canDo(prefix, plen)) {
+                return false;
+            }
+            final int nextSidx = sidx + 1;
+            if (nextSidx < slen) {
+                for (int next : allowed[suffix[sidx]][suffix[nextSidx]]) {
+                    prefix[plen] = next;
+                    if (compute(prefix, plen + 1, suffix, sidx + 1, slen)) {
+                        return true;
+                    }
+                }
+            } else {
+                return canDo(prefix, plen);
+            }
+            return false;
         }
     }
 }
