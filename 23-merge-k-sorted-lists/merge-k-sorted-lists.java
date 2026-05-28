d853216d9f1f3c0123f6b37717806b4e20ab5479
Rishi Singh <rishipratap6698@gmail.com>
2026-05-28 11:25:53 +0530

    Time: 1 ms (99.73%) | Memory: 46.8 MB (63.24%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,38 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
-        
-        for (ListNode list : lists) {
-            if (list != null) {
-                minHeap.add(list);
-            }
+        return dc(lists, 0, lists.length - 1);
+    }
+
+    private ListNode dc(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
         }
         
+        int mid = start + (end - start) / 2;
+        ListNode left = dc(lists, start, mid);
+        ListNode right = dc(lists, mid + 1, end);
+        
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
         
-        while (!minHeap.isEmpty()) {
-            ListNode lowest = minHeap.poll();
-            tail.next = lowest;
-            tail = tail.next;
-            
-            if (lowest.next != null) {
-                minHeap.add(lowest.next);
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
             }
+            tail = tail.next;
+        }
+        
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
         }
         
         return dummy.next;
